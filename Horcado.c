ebb3f59f8b8ae2c7cb05ab7d06716f46401aaeeb
MrDeckMacc <jorge.martinez35@uabc.edu.mx>
2022-03-17 20:26:20 -0700

    puse los datos que cambie en la mañana

--- Horcado.c
+++ Horcado.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include "ventanas.h"
 
+int InterfazInicio();
+
 int main()
 {
+	InterfazInicio();
+	//Interfaz General
+	
+	//
+	
+	system("cls");
 
-	//system("clear");
+	return 0;
+}
+
+int InterfazInicio(void)
+{
+		int opc;
 	// Este es el cuadro principal
-	Recuadro(1, 1, 82, 22, 2);
-	Recuadro(1, 1, 82, 22, 2);
-	Recuadro(1, 1, 82, 22, 2);
+	Recuadro(0, 0, 82, 22, 2);
+	//Recuadro(1, 1, 82, 22, 2);
+	//Recuadro(1, 1, 82, 22, 2);
 	// Los textos centrados en el cuadro
-	LineaHorizontal(1, 82, 3, 2);
-	LineaHorizontal(1, 82, 20, 2);
+	LineaHorizontal(0, 82, 3, 2);
+	LineaHorizontal(0, 82, 20, 2);
 	PonTextoCentradoPantallaAnimadoVERT(2, 10, "Juego Del Horcado");
 	// PonTextoCentradoPantalla(2, "JUEGO DEL HORCADO");
 	PonTextoCentradoPantallaDELAY(20, "[MENSAJES]");
@@ -23,11 +36,23 @@ int main()
 	PonTextoCentradoPantallaDELAY(10, "PAISES-----------[2]");
 	PonTextoCentradoPantallaDELAY(11, "DEPORTES---------[3]");
 	PonTextoCentradoPantallaDELAY(12, "SALIR------------[4]");
-	Recuadro(1, 1, 82, 22, 2);
-	Recuadro(1, 1, 82, 22, 2);
+	PonTextoCentradoPantallaDELAY(13,"Selecciona-------[ ]");
 	// Escaner el numero de opcion
-
+	do
+	{
+		gotoxy(48,13);
+		scanf("%d",&opc);
+		//if(opc==4)
+		//{
+		//	return 0;
+		//}
+		if(opc<1||opc>4)
+		{
+			PonTextoCentradoPantallaDELAY(21,"OPCION NO VALIDA");
+		}	
+	}while(opc<1||opc>4);
 	// El salto para que no se coma el cuadro el mensaje de abajo
 	scanf("%d");
-	return 0;
+	return opc;
 }
+
